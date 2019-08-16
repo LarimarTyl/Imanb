9f220623e3d8efb0fe47e1198d3a9330a1d9a13a
LarimarTyl <565203943@qq.com>
2019-08-16 17:54:50 +0800

    js文件-tyl

--- js/my.js
+++ js/my.js
@@ -5,3 +5,19 @@
         })
     });
 // 轮播图js
+// 分类列表选择案例
+    $(function(){
+        $("#book-list .list li").mouseenter(function(){
+            $(this).find("div").addClass("list-select").addClass("up").parents("li").siblings().find("div").removeClass("list-select").removeClass("up")
+        }).click(function () {
+            $(this).find("div").addClass("list-active").parents("li").siblings().find("div").removeClass("list-active")
+        })
+        });
+// 分类列表选择案例
+//漫画项选择案例
+    $(function () {
+       $(".book-item li").mouseenter(function () {
+           $(this).toggleClass("more-up")
+       })
+    });
+//漫画项选择案例
